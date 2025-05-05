f221b54b980992fee8c7545ff9058d5dbe86fe3e
saltyaom <saltyaom@gmail.com>
2025-05-05 21:48:12 +0700

    :broom: chore: bump version

--- example/index.ts
+++ example/index.ts
@@ -1,34 +1,28 @@
 import { Elysia } from 'elysia'
 import { apollo } from '../src/index'
-import { cors } from '@elysiajs/cors'
 
 import typeDefs from './schema'
 import resolvers from './resolvers'
 
 interface MyContext {
-    hi: string
+	hi: string
 }
 
 const app = new Elysia()
-    .use(
-        cors({
-            origin: 'http://localhost'
-        })
-    )
-    .use(
-        apollo<'/graphql', MyContext>({
-            path: '/graphql',
-            typeDefs,
-            resolvers,
-            enablePlayground: true,
-            context: async (context) => {
-                return { hi: 'there' }
-            }
-        })
-    )
-    .get('/', () => 'hi')
-    .listen(3000)
+	.use(
+		apollo<'/graphql', MyContext>({
+			path: '/graphql',
+			typeDefs,
+			resolvers,
+			enablePlayground: true,
+			context: async (context) => {
+				return { hi: 'there' }
+			}
+		})
+	)
+	.get('/', () => 'hi')
+	.listen(3000)
 
 console.log(
-    `🦊 Now foxing at http://${app.server?.hostname}:${app.server?.port}`
+	`🦊 Now foxing at http://${app.server?.hostname}:${app.server?.port}`
 )
